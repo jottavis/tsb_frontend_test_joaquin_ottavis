19a0ffa495e1c4d4113d6129962b59eb2a13359d
jottavis <joacotta@gmail.com>
2023-12-14 14:29:58 -0300

    Doggie Detail styling

--- src/components/DoggieDetail.vue
+++ src/components/DoggieDetail.vue
@@ -3,13 +3,13 @@
 
 <template>
     <section class="text-white">
-      <div class=" container px-4 ">
+      <div class=" container lg:max-w-5xl mx-auto md:grid md:grid-cols-2  gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
             <img
                 src="/doggie.avif"
                 alt="doggie"
                 class=" "
             />
-            <div class="mt-4">
+            <div class="mt-4 md:mt-0 text-left flex flex-col h-full">
                 <h2 class="text-4xl font-bold text-white mb-4">Doggy #7312</h2>
                 <small>Owner :</small>
                 <label>0x35F1bAe79c4E74eE25354D326B42F809744F7E18</label>
